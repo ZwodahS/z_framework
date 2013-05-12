a74c130ad726f3727bef37121af2e23d7d31a235
Eric Ng(ZwodahS) <ericnjf@gmail.com>
2013-05-12 23:33:09 +0800

    Added Direction.hpp

---
+++ zf_common/Direction.hpp
@@ -0,0 +1,18 @@
+#ifndef _ZF_FRAMEWORK_DIRECTION_H_
+#define _ZF_FRAMEWORK_DIRECTION_H_
+
+namespace zf
+{
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West,
+        NorthEast,
+        SouthEast,
+        SouthWest,
+        NorthWest,
+    };
+}
+#endif
